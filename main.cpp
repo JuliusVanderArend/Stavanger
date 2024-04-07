2b7caa97b0bb4bd5330e79151d3de8c899b164d8
Julius Vander Arend <julius.vanderarend@student.manchester.ac.uk>
2024-04-07 19:30:17 +0100

    added benchmark for movegen

--- main.cpp
+++ main.cpp
@@ -3,6 +3,7 @@
 
 
 #include <iostream>
+#include <chrono>
 #include "Position.h"
 #include "MoveGenerator.h"
 #include "Helper.h"
@@ -14,7 +15,7 @@ int main() {
     std::cout << "Hello, World!" << std::endl;
 
     Position* pos = new Position();
-    pos->whiteToMove = false;
+    pos->whiteToMove = true;
 
     pos->draw();
 //    pos->debugDraw("occupancy");
@@ -23,7 +24,18 @@ int main() {
 
     std::vector<Move> generatedMoves;
     MoveGenerator* moveGen = new MoveGenerator();
-    generatedMoves = moveGen->generateMoves(pos);
+
+    auto start = std::chrono::high_resolution_clock::now();
+    for (int i = 0; i < 1000000; ++i) {
+        moveGen->generateMoves(pos);
+    }
+
+    // Stop the timer
+    auto end = std::chrono::high_resolution_clock::now();
+
+    // Calculate the duration
+    std::chrono::duration<double> duration = end - start;
+    std::cout << "Time taken: " << duration.count() << " seconds" << std::endl;
 
     std::cout << generatedMoves.size() << std::endl;
     for(Move move : generatedMoves){
